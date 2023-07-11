a474f013023e0ef9be089d0d13b779f69bd7da36
connie kennedy <ckennedy8728@conestogac.on.ca>
2023-07-11 15:09:14 -0400

    Add email template for appointment reminders and logic for when to send reminder emails.

--- app/Http/Traits/FilterByDate.php
+++ app/Http/Traits/FilterByDate.php
@@ -8,6 +8,16 @@ trait FilterByDate
 {
     public function scopeToday($query)
     {
-        return $query->whereDate('appointment_date', Carbon::today());
+        return $query->where('appointment_date', Carbon::today());
+    }
+
+    public function scopeTomorrow($query)
+    {
+        return $query->where('appointment_date', Carbon::tomorrow());
+    }
+
+    public function scopeYesterday($query)
+    {
+        return $query->where('appointment_date', Carbon::yesterday());
     }
 }
---
+++ resources/views/emails/appointment/reminder.blade.php
@@ -0,0 +1,12 @@
+<x-mail::message>
+# Appointment Reminder
+
+    This is a reminder for {{$appointment->dog->name}}'s {{$appointment->appointmentable_type}} appointment for tomorrow, {{$appointment->appointment_date->toDateString()}}! If you want to make changes, feel free to call us at {{$appointment->facility->phone_number}}
+
+<x-mail::button :url="$url">
+    View your appointment
+</x-mail::button>
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
