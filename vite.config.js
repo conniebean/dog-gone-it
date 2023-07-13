20fef1436074f1438ef59d03481430ed78d36a44
conniebean <77754785+conniebean@users.noreply.github.com>
2023-07-13 08:49:14 -0400

    add vitest to the project (#18)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+
+
 export default defineConfig({
+    test: {
+        //
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
